02cb9f06e8fea60c91c271c9750b9e8ddd6ec57f
kenjiushiro <kenjimartinushiro@gmail.com>
2022-01-24 17:51:44 -0300

    Add mac alt tab

--- keymap.c
+++ keymap.c
@@ -23,14 +23,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "oled.c"
 #endif
 
-bool is_alt_tab_active = false; // ADD this near the begining of keymap.c
+bool is_alt_tab_active = false;
+bool is_alt_tab_mac_active = false;
 uint16_t alt_tab_timer = 0;
+uint16_t alt_tab_mac_timer = 0;
 
 enum layers {
   _QWERTY,
   _QWERTY_MAC,
   _NUMERIC,
   _NAVIGATION,
+  _NAVIGATION_MAC,
   _SYMBOLS,
   _FN_KEYS,
   _CONFIG,
@@ -42,7 +45,9 @@ enum custom_keycodes  {
     DEVARTIS_EMAIL,
     TIENDANUBE_EMAIL,
     ALT_TAB,
-    LSALT_TAB
+    LSALT_TAB,
+    ALT_TAB_MAC,
+    LSALT_TAB_MAC
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -54,7 +59,7 @@ LCTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                              MO(_NAVIGATION),MO(_SYMBOLS),  KC_SPC,    KC_LGUI, MO(_FN_KEYS), KC_LALT
+                              MO(_NAVIGATION_MAC),MO(_SYMBOLS),  KC_SPC,    KC_LGUI, MO(_FN_KEYS), KC_LALT
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -96,6 +101,18 @@ LGUI_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
                                       //`--------------------------'  `--------------------------'
   ),
 
+  [_NAVIGATION_MAC] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      LSALT_TAB_MAC,  ALT_TAB_MAC, KC_INS, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_HOME,  KC_END, XXXXXXX, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_PSCR, KC_BSPC, KC_LCTL, KC_LSFT,  KC_ENT, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, KC_DEL,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LGUI, XXXXXXX, XXXXXXX,   KC_DEL, XXXXXXX,XXXXXXX,                      XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, XXXXXXX, KC_ENT,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          _______, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX
+                                      //`--------------------------'  `--------------------------'
+  ),
+
   [_SYMBOLS] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       LSFT(KC_GRV),LSFT(KC_1),LSFT(KC_5),LSFT(KC_3),LSFT(KC_8),XXXXXXX,         XXXXXXX, XXXXXXX,LSFT(KC_LBRC),LSFT(KC_RBRC),XXXXXXX,KC_BSPC,
@@ -121,7 +138,7 @@ LGUI_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
 
   [_CONFIG] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, KC_MS_U, XXXXXXX, DF(_QWERTY), DF(_QWERTY_MAC),          KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, LIVE_EMAIL, GMAIL_EMAIL,
+      CG_SWAP, XXXXXXX, KC_MS_U, XXXXXXX, DF(_QWERTY), DF(_QWERTY_MAC),          KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, LIVE_EMAIL, GMAIL_EMAIL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, TIENDANUBE_EMAIL, DEVARTIS_EMAIL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -186,15 +203,47 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         unregister_code(KC_TAB);
       }
       break;
+    case ALT_TAB_MAC:
+      if (record->event.pressed) {
+        if (!is_alt_tab_mac_active) {
+          is_alt_tab_mac_active = true;
+          register_code(KC_LGUI);
+        }
+        alt_tab_mac_timer = timer_read();
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+      }
+      break;
+    case LSALT_TAB_MAC:
+      if (record->event.pressed) {
+        if (!is_alt_tab_mac_active) {
+          is_alt_tab_mac_active = true;
+          register_code(KC_LGUI);
+        }
+        alt_tab_mac_timer = timer_read();
+        register_code(KC_LSFT);
+        register_code(KC_TAB);
+        unregister_code(KC_LSFT);
+      } else {
+        unregister_code(KC_TAB);
+      }
+      break;
   }
   return true;
 }
 
 void matrix_scan_user(void) { // The very important timer.
   if (is_alt_tab_active) {
-    if (timer_elapsed(alt_tab_timer) > 500) {
+    if (timer_elapsed(alt_tab_timer) > 866) {
       unregister_code(KC_LALT);
       is_alt_tab_active = false;
+    }
+  }
+  if (is_alt_tab_mac_active) {
+    if (timer_elapsed(alt_tab_mac_timer) > 866) {
+      unregister_code(KC_LGUI);
+      is_alt_tab_mac_active = false;
     }
   }
 }
