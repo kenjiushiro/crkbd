b2f1912952d2883a65563ac1cc9261b856b28043
Kenji Ushiro <kenjiushiro@pop-os.localdomain>
2022-11-03 19:20:39 -0300

    Tweak numeric layer

--- keymap.c
+++ keymap.c
@@ -114,11 +114,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUMERIC] = LAYOUT_split_3x6_3(
             //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-               _______, KC_TAB,  XXXXXXX, XXXXXXX, ASTERISK,XXXXXXX,                      XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX, KC_BSPC,
+               _______, KC_TAB,  XXXXXXX, XXXXXXX, ASTERISK,XXXXXXX,                      KC_EQL, KC_7,    KC_8,    KC_9,    XXXXXXX, KC_BSPC,
             //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      PLUS_SIGN,KC_4,   KC_5,    KC_6,    KC_MINUS, KC_DEL,
             //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-               KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_1,    KC_2,    KC_3,    KC_SLSH, KC_ENT,
+               KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      ASTERISK,KC_1,    KC_2,    KC_3,    KC_SLSH, KC_ENT,
             //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                                     KC_COMM,  KC_DOT,  KC_SPC,    KC_LGUI,    KC_0, XXXXXXX
             //`--------------------------'  `--------------------------'
